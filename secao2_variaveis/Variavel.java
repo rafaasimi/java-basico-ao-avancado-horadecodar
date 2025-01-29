3020d81fbbe839d9aaf2feff951ac61039db7570
Rafael Simionato <rafaasimi@hotmail.com>
2025-01-29 13:48:53 -0300

    aula: dados primitivos e atribuição de valor com outra variavel

---
+++ secao2_variaveis/Variavel.java
@@ -0,0 +1,27 @@
+package secao2_variaveis;
+
+public class Variavel {
+
+    public static void main(String[] args) {
+
+        // 1. O que são variáveis
+        // tipo -> nome -> atribuir valor
+        String nome = "Rafael";
+
+        // nome -> "Rafael"
+        System.out.println(nome);
+
+        // 2. Atribuição de variável com outra
+        String teste = "Teste";
+        String testando = teste;
+
+        System.out.println(testando);
+
+        long numeroGrande = 999999999999999999L;
+
+        // int numeroGrande2 = numeroGrande;
+        System.out.println(numeroGrande);
+
+    }
+
+}
