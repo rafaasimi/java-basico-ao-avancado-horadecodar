60f30583be137fa29d348cb856da78ff1089a6f6
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-04 12:42:14 -0300

    aula: constantes

---
+++ secao2_variaveis/Constantes.java
@@ -0,0 +1,14 @@
+package secao2_variaveis;
+
+public class Constantes {
+
+    public static void main(String[] args) {
+
+        // 12. Constantes
+        final int DIAS_DA_SEMANA = 7;
+
+        System.out.println("Dias da semana: " + DIAS_DA_SEMANA);
+
+    }
+
+}
