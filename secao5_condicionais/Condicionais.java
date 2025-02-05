11dea34037feea767a025cd6ffa6c1cad79cabb0
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-05 08:54:26 -0300

    aula: o que é boolean?

---
+++ secao5_condicionais/Condicionais.java
@@ -0,0 +1,16 @@
+package secao5_condicionais;
+
+public class Condicionais {
+
+    public static void main(String[] args) {
+
+        // 1. O que é boolean?
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+
+    }
+
+}
