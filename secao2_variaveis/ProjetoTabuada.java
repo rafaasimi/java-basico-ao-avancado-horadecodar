724a9f22983814588a65201309e8d5ac6428630d
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-05 08:35:59 -0300

    aula: projeto tabuada

---
+++ secao2_variaveis/ProjetoTabuada.java
@@ -0,0 +1,30 @@
+package secao2_variaveis;
+
+import java.util.Scanner;
+
+// Projeto Tabuada
+// Receber um número pelo scanner e exibir a tabuada do mesmo.
+public class ProjetoTabuada {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual a tabuada você quer saber? ");
+        int numero = scanner.nextInt();
+
+        System.out.println(numero + "x1=" + (numero * 1));
+        System.out.println(numero + "x2=" + (numero * 2));
+        System.out.println(numero + "x3=" + (numero * 3));
+        System.out.println(numero + "x4=" + (numero * 4));
+        System.out.println(numero + "x5=" + (numero * 5));
+        System.out.println(numero + "x6=" + (numero * 6));
+        System.out.println(numero + "x7=" + (numero * 7));
+        System.out.println(numero + "x8=" + (numero * 8));
+        System.out.println(numero + "x9=" + (numero * 9));
+        System.out.println(numero + "x10=" + (numero * 10));
+
+        scanner.close();
+    }
+
+}
