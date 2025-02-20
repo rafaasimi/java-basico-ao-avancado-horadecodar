c7b1249cae7c92decfa564475253de35b3a14eb2
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-20 11:56:16 -0300

    aula: condicional ternaria

---
+++ secao5_condicionais/CondicionaisAvancadas.java
@@ -0,0 +1,16 @@
+package secao5_condicionais;
+
+public class CondicionaisAvancadas {
+
+    public static void main(String[] args) {
+
+        // 1. Condicional ternária
+        int numero = 10;
+
+        String resultado = (numero % 2 == 0) ? "Par" : "Ímpar";
+
+        System.out.println(resultado);
+
+    }
+
+}
