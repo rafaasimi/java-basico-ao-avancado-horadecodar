a8c5bd4874f6214abad98939d5ee630f65a3dad2
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-24 09:14:49 -0300

    aula: argumentos em função

--- secao8_funcoes/Funcoes.java
+++ secao8_funcoes/Funcoes.java
@@ -4,6 +4,7 @@ public class Funcoes {
 
     public static void main(String[] args) {
         saudacao();
+        soma(2, 4);
     }
 
     // 1. Criando a primeira função
@@ -12,6 +13,12 @@ public class Funcoes {
     // void -> a função não irá ter retorno
     public static void saudacao() {
         System.out.println("Olá, está é a minha primeira função.");
+    }
+
+    // 2. Parâmetros
+    public static void soma(int a, int b) {
+        int resultado = a + b;
+        System.out.println("O resultado da soma é: " + resultado);
     }
 
 }
