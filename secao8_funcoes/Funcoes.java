2dab34bcb40d5305d08f11eb1b07f6ffb6620b6e
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-25 08:18:56 -0300

    aula: retorno de função

--- secao8_funcoes/Funcoes.java
+++ secao8_funcoes/Funcoes.java
@@ -3,22 +3,33 @@ package secao8_funcoes;
 public class Funcoes {
 
     public static void main(String[] args) {
-        saudacao();
+        saudar("Rafael");
         soma(2, 4);
+
+        int numero = 32;
+        int numeroDobrado = dobrar(numero);
+        System.out.println("Numero dobrado: " + numeroDobrado);
+        System.out.println(dobrar(14));
+
     }
 
     // 1. Criando a primeira função
     // public -> nivel de acesso
     // static -> não precisa instanciar a classe para executar
     // void -> a função não irá ter retorno
-    public static void saudacao() {
-        System.out.println("Olá, está é a minha primeira função.");
+    public static void saudar(String nome) {
+        System.out.println("Olá " + nome + ", seja bem-vindo.");
     }
 
     // 2. Parâmetros
     public static void soma(int a, int b) {
         int resultado = a + b;
         System.out.println("O resultado da soma é: " + resultado);
+    }
+
+    // 3. Return
+    public static int dobrar(int n) {
+        return n * 2;
     }
 
 }
