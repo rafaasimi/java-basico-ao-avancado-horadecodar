d053e364d4fa800dcafc3aa13e3e8a229093a571
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-17 08:42:42 -0300

    aula: método de overloading

--- secao8_funcoes/FuncoesAvancadas.java
+++ secao8_funcoes/FuncoesAvancadas.java
@@ -7,6 +7,10 @@ public class FuncoesAvancadas {
         // 12. Função recursiva
         System.out.println(somaRecursiva(5));
 
+        // 13. Método de Overloading
+        System.out.println(soma(1, 2));
+        System.out.println(soma(10, 20, 30));
+        System.out.println(soma(15.5, 18.5));
     }
 
     public static int somaRecursiva(int numero) {
@@ -15,6 +19,18 @@ public class FuncoesAvancadas {
         } else {
             return numero + somaRecursiva(numero - 1);
         }
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int soma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static double soma(double a, double b) {
+        return a + b;
     }
 
 }
