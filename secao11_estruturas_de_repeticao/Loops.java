9838abdb17a2bfb357cb93d71e1188b294f2b2b7
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-21 08:11:29 -0300

    aula: conhecendo o for

---
+++ secao11_estruturas_de_repeticao/Loops.java
@@ -0,0 +1,32 @@
+package secao11_estruturas_de_repeticao;
+
+public class Loops {
+
+  public static void main(String[] args) {
+
+    // 1. For
+    // Loop que irá rodar de 1 a 5
+    // Variavel de inicialização: int i = 0, int j = 1
+    // Condição: determina até quando ou quantas vezes o loop irá executar
+    // Incremento ou Decremento: Forma de fazer a váriavel chegar a condição
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("Contador: " + i);
+    }
+
+    // Mostrar cada caractere da String
+    String palavra = "Rafael";
+
+    // Arrays e Strings a primeira posição é 0
+    for (int i = 0; i < palavra.length(); i++) {
+      System.out.println("Caractere: " + palavra.charAt(i));
+    }
+
+    // Contagem regressiva
+    for (int i = 10; i >= 0; i--) {
+      System.out.println("Contagem regressiva: " + i);
+    }
+
+
+  }
+
+}
