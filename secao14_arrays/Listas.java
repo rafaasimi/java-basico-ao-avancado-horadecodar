d9b1c8cf9bed3e2e79160c739e2195face80a8af
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-16 08:24:21 -0300

    aula: utilizando o for-each

--- secao14_arrays/Listas.java
+++ secao14_arrays/Listas.java
@@ -1,12 +1,12 @@
 package secao14_arrays;
 
 public class Listas {
-  
+
   public static void main(String[] args) {
-    
+
     // 1. Sintaxe
     // {elemento1, elemento2, ..., elementoN}
-    int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8};
+    int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
     System.out.println("Acessando o primeiro elemento: " + numeros[0]);
     System.out.println("Acessando o último elemento: " + numeros[numeros.length - 1]);
@@ -26,18 +26,17 @@ public class Listas {
 
     // 2. Loops em arrays
     int soma = 0;
-    for(int i = 0; i < numeros.length; i++) {
+    for (int i = 0; i < numeros.length; i++) {
       soma += numeros[i];
     }
     System.out.println("A soma do array de números é: " + soma);
 
-    
     for (String fruta : frutas) {
       System.out.println("A fruta da vez é: " + fruta);
     }
 
     // Encontrar o maior valor de um array
-    int[] valores = {10, 25, 8, 22, 1};
+    int[] valores = { 10, 25, 8, 22, 1 };
     int maiorValor = valores[0];
 
     int j = 0;
@@ -47,6 +46,33 @@ public class Listas {
     }
 
     System.out.println("O maior valor do array é: " + maiorValor);
+
+    // 3. For-each
+    for (int numero : numeros) {
+      System.out.println("O número é: " + numero);
+    }
+
+    // Concatenar elementos de um arrau
+    String[] palavras = { "Java", "é", "bom!" };
+    String frase = "";
+
+    for (String palavra : palavras) {
+      frase += palavra + " ";
+    }
+
+    System.out.println(frase);
+
+    // Verificar se valor está presente no array
+    char[] letras = { 'a', 'e', 'i', 'o', 'u' };
+    char letraProcurada = 'i';
+
+    for (char letra : letras) {
+      if (letra == letraProcurada) {
+        System.out.println("A letra " + letraProcurada + " está presente no array.");
+        break;
+      }
+    }
+
   }
 
 }
